c59e98910ea2625918e5a7eb9cd76ef7413bd044
fbi178 <45146475+fbi178@users.noreply.github.com>
2026-06-02 01:02:19 +0800

    fix

--- AppListController.m
+++ AppListController.m
@@ -7,34 +7,17 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
     self.title = @"应用";
     self.navigationController.navigationBar.translucent = NO;
-    
-    // 刷新按钮
-    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemRefresh
-                                                                                           target:self
-                                                                                           action:@selector(reloadApps)];
-    
-    // 搜索
-    self.searchController = [[UISearchController alloc] initWithSearchResultsController:nil];
-    self.searchController.searchResultsUpdater = self;
-    self.searchController.obscuresBackgroundDuringPresentation = NO;
-    self.navigationItem.searchController = self.searchController;
-    self.definesPresentationContext = YES;
-    
-    // 注册 Cell
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemRefresh target:self action:@selector(reloadApps)];
     [self.tableView registerClass:[AppTableViewCell class] forCellReuseIdentifier:@"AppCell"];
     self.tableView.rowHeight = 64;
     self.tableView.tableFooterView = [[UIView alloc] init];
-    
-    // 加载应用列表
     [self loadApps];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
-    [self loadApps];
 }
 
 - (void)loadApps {
@@ -47,51 +30,24 @@
     });
 }
 
-- (void)reloadApps {
-    [self loadApps];
-}
+- (void)reloadApps { [self loadApps]; }
 
-#pragma mark - TableView DataSource
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 1;
-}
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView { return 1; }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return self.isFiltering ? self.filteredApps.count : self.apps.count;
+    return self.apps.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     AppTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"AppCell" forIndexPath:indexPath];
-    ApplicationItem *item = self.isFiltering ? self.filteredApps[indexPath.row] : self.apps[indexPath.row];
-    [cell configureWithItem:item];
+    [cell configureWithItem:self.apps[indexPath.row]];
     return cell;
 }
 
-#pragma mark - TableView Delegate
-
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
-    ApplicationItem *item = self.isFiltering ? self.filteredApps[indexPath.row] : self.apps[indexPath.row];
-    AppDetailController *detailVC = [[AppDetailController alloc] initWithAppItem:item];
+    AppDetailController *detailVC = [[AppDetailController alloc] initWithAppItem:self.apps[indexPath.row]];
     [self.navigationController pushViewController:detailVC animated:YES];
-}
-
-#pragma mark - Search
-
-- (BOOL)isFiltering {
-    return self.searchController.isActive && self.searchController.searchBar.text.length > 0;
-}
-
-- (void)updateSearchResultsForSearchController:(UISearchController *)searchController {
-    NSString *text = searchController.searchBar.text;
-    if (!self.apps || text.length == 0) {
-        self.filteredApps = nil;
-    } else {
-        NSPredicate *predicate = [NSPredicate predicateWithFormat:@"appName CONTAINS[cd] %@ OR bundleId CONTAINS[cd] %@", text, text];
-        self.filteredApps = [self.apps filteredArrayUsingPredicate:predicate];
-    }
-    [self.tableView reloadData];
 }
 
 @end
