ec16c631f562d71655923d82f6443cae517a6235
fbi178 <45146475+fbi178@users.noreply.github.com>
2026-06-01 09:23:41 +0800

    Add ApplicationItem.h

---
+++ ApplicationItem.h
@@ -0,0 +1,24 @@
+#import <UIKit/UIKit.h>
+
+@interface ApplicationItem : NSObject
+
+@property (nonatomic, copy) NSString *bundleId;
+@property (nonatomic, copy) NSString *appName;
+@property (nonatomic, copy) NSString *version;
+@property (nonatomic, strong) UIImage *icon;
+@property (nonatomic, copy) NSString *dataContainerPath;
+@property (nonatomic, strong) NSArray *groupContainerPaths;
+@property (nonatomic, assign) BOOL isSystemApp;
+@property (nonatomic, assign) BOOL isUserApp;
+
+- (instancetype)initWithBundleId:(NSString *)bundleId
+                         appName:(NSString *)appName
+                         version:(NSString *)version
+                            icon:(UIImage *)icon
+               dataContainerPath:(NSString *)dataContainerPath
+              groupContainerPaths:(NSArray *)groupContainerPaths
+                      isSystemApp:(BOOL)isSystemApp;
+
++ (NSArray<ApplicationItem *> *)allInstalledApps;
+
+@end
